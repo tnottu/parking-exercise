8e20a4366cf2008667f6e73daf1ba9f021b5e1c7
Tomi <tnottu@gmail.com>
2021-10-24 21:24:50 +0300

    Remove dummy data from backend

--- backend/schemas/carParks.ts
+++ backend/schemas/carParks.ts
@@ -1,29 +1,6 @@
 const { gql } = require('apollo-server-express');
 const oukaMiddleman = require('../services/ouka-middleman')
 
-const _carParksMockData = [
-  {
-    carParkId: '3d599470',
-    name: 'Testiparkki',
-    maxCapacity: 100,
-    spacesAvailable: 65,
-    realtime: false,
-    pricing: null,
-    lon: 0,
-    lat: 0,
-  },
-  {
-    carParkId: '570as345',
-    name: 'Parkkitesti',
-    maxCapacity: 154,
-    spacesAvailable: 21,
-    realtime: false,
-    pricing: null,
-    lon: 0,
-    lat: 0,
-  },
-]
-
 const typeDefs = gql`
   type CarPark {
     carParkId: String
@@ -58,7 +35,6 @@ const resolvers = {
   Query: {
     carParks: async (_parent:any, _args:any, _context:any) => {
       const response = await oukaMiddleman.query(_context.req)
-      // return _carParksMockData
       return response.data.carParks
     },
   }
